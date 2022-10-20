32b5392c593b719e5acfda2adb48825e17a104ea
lewisc99 <lewiscontato99@gmail.com>
2022-10-20 13:29:29 -0300

    10. Test Case Lifecycle Methods - Coding

--- junitdemo/src/test/java/com/luv2code/junitdemo/DemoUtilsTests.java
+++ junitdemo/src/test/java/com/luv2code/junitdemo/DemoUtilsTests.java
@@ -1,16 +1,61 @@
 package com.luv2code.junitdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTests {
 
+    private static final Logger logger = Logger.getLogger(DemoUtilsTests.class.getName());
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach()
+    {
+         demoUtils = new DemoUtils();
+        logger.info("@BeforeEach: executes before the execution of each test method");
+
+    }
+
+
+    @AfterEach
+    void teardownAfterEach()
+    {
+        logger.info("Running @AfterEach Method");
+    }
+
+
+
+    @BeforeAll
+    static //executed only once, before all test methods
+    void setupBeforeEachClass()
+    {
+        logger.info("@BeforeAll executes only before all test methods");
+
+    }
+
+
+    @AfterAll
+    static //executed only once, after all test methods
+    void tearDownAfterAll()
+    {
+        logger.info("@AfterAll executes only After all test methods");
+
+    }
+
 
      @Test
      void testEqualsAndNotEquals()
      {
-         DemoUtils demoUtils = new DemoUtils();
+
+
+         logger.info("Running test:  testEqualsAndNotEquals");
+
+      //   DemoUtils demoUtils = new DemoUtils();
 
          assertEquals(6, demoUtils.add(2,4), "2 + 4 must be 6");
          assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
@@ -21,7 +66,10 @@ class DemoUtilsTests {
      @Test
      void testNullAndNotNull()
      {
-         DemoUtils demoUtils = new DemoUtils();
+
+         logger.info("Running test:  testNullAndNotNull");
+
+        // DemoUtils demoUtils = new DemoUtils();
 
 
          String str1 = null;

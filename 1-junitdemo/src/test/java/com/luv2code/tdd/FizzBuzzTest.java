46461e04d4048d14e817484fcf69b3353e39e155
lewisc99 <lewiscontato99@gmail.com>
2022-10-26 11:50:39 -0300

    36. FizzBuzz Project - Coding - Getting Started

---
+++ 1-junitdemo/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+package com.luv2code.tdd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FizzBuzzTest {
+
+    // if number is divisible by 3, print Fizz
+    // if number is divisible by 5, print buzz
+    //if number is divisible by 3 and 5. print FizzBuzz
+    //if number is not divisible by 3 and 5, then print the number
+
+
+    @Test
+    @DisplayName("Divisible by three")
+    void testForDivisibleByThree()
+    {
+        fail("Fail");
+    }
+
+}

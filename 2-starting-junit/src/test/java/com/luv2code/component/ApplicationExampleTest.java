c055aa2c3f128846d38c4e2ae68eadf738d667d3
lewisc99 <lewiscontato99@gmail.com>
2022-10-27 18:40:02 -0300

    46. Spring Boot Unit Testing - Adding Maven Dependency

---
+++ 2-starting-junit/src/test/java/com/luv2code/component/ApplicationExampleTest.java
@@ -0,0 +1,16 @@
+package com.luv2code.component;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class ApplicationExampleTest {
+
+    @Test
+    void basicTest()
+    {
+
+
+    }
+}

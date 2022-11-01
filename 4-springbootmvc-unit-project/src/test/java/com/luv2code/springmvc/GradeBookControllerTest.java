0785f67f393a9b9efafee69ebce08238c6d82360
lewisc99 <lewiscontato99@gmail.com>
2022-11-01 08:10:49 -0300

    77. Testing Spring MVC Web Controllers - Test Setup

---
+++ 4-springbootmvc-unit-project/src/test/java/com/luv2code/springmvc/GradeBookControllerTest.java
@@ -0,0 +1,46 @@
+package com.luv2code.springmvc;
+
+
+import com.luv2code.springmvc.Service.StudentAndGradeService;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@TestPropertySource("/application.properties")
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GradeBookControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private StudentAndGradeService studentAndGradeService;
+
+    @BeforeEach
+    public void beforeEach()
+    {
+        jdbc.execute("insert into student(id, firstname, lastname, email_address) " +
+                "values (1, 'Eric','Roby', 'eric.roby@luv2code_school.com')");
+    }
+
+
+    @AfterEach
+    public void setupAfterTransaction()
+    {
+        jdbc.execute("DELETE FROM student");
+
+    }
+
+}

6e92818368f39f093da78ad7e1bf491fc137b81a
lewisc99 <lewiscontato99@gmail.com>
2022-11-04 08:37:18 -0300

    109. Testing Spring Boot MVC Web Apps - Test Create Grade for Invalid Student ID

--- 4-springbootmvc-unit-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ 4-springbootmvc-unit-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -33,29 +33,8 @@ public class GradebookController {
             return "error";
         }
 
-        GradebookCollegeStudent studentEntity = studentService.studentInformation(id);
-        m.addAttribute("student", studentEntity);
-        if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
-            m.addAttribute("mathAverage", studentEntity.getStudentGrades().findGradePointAverage(
-                    studentEntity.getStudentGrades().getMathGradeResults()
-            ));
-        } else {
-            m.addAttribute("mathAvarage", "N/A");
-        }
-        if (studentEntity.getStudentGrades().getScienceGradeResults().size() > 0) {
-            m.addAttribute("scienceAverage", studentEntity.getStudentGrades().findGradePointAverage(
-                    studentEntity.getStudentGrades().getScienceGradeResults()
-            ));
-        } else {
-            m.addAttribute("scienceAverage", "N/A");
-        }
-        if (studentEntity.getStudentGrades().getHistoryGradeResults().size() > 0) {
-            m.addAttribute("historyAverage", studentEntity.getStudentGrades().findGradePointAverage(
-                    studentEntity.getStudentGrades().getHistoryGradeResults()
-            ));
-        } else {
-            m.addAttribute("historyAverage", "N/A");
-        }
+        studentService.configureStudentInformation(id,m);
+
         return "studentInformation";
     }
 
@@ -97,30 +76,9 @@ public class GradebookController {
         if (!success) {
             return "error";
         }
-        GradebookCollegeStudent studentEntity = studentService.studentInformation(studentId);
 
-        m.addAttribute("student", studentEntity);
-        if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
-            m.addAttribute("mathAverage", studentEntity.getStudentGrades().findGradePointAverage(
-                    studentEntity.getStudentGrades().getMathGradeResults()
-            ));
-        } else {
-            m.addAttribute("mathAvarage", "N/A");
-        }
-        if (studentEntity.getStudentGrades().getScienceGradeResults().size() > 0) {
-            m.addAttribute("scienceAverage", studentEntity.getStudentGrades().findGradePointAverage(
-                    studentEntity.getStudentGrades().getScienceGradeResults()
-            ));
-        } else {
-            m.addAttribute("scienceAverage", "N/A");
-        }
-        if (studentEntity.getStudentGrades().getHistoryGradeResults().size() > 0) {
-            m.addAttribute("historyAverage", studentEntity.getStudentGrades().findGradePointAverage(
-                    studentEntity.getStudentGrades().getHistoryGradeResults()
-            ));
-        } else {
-            m.addAttribute("historyAverage", "N/A");
-        }
+        studentService.configureStudentInformation(studentId,m);
+
         return "studentInformation";
     }
 

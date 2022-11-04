8c2484100a901b67818a72a80c24907176d23e4e
lewisc99 <lewiscontato99@gmail.com>
2022-11-04 08:09:34 -0300

    107. Testing Spring Boot MVC Web Apps - MVC Tests for Create Grades - Passing Test

--- 4-springbootmvc-unit-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
+++ 4-springbootmvc-unit-project/src/main/java/com/luv2code/springmvc/controller/GradebookController.java
@@ -21,7 +21,7 @@ public class GradebookController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getStudents(Model m) {
         Iterable<CollegeStudent> collegeStudents = studentService.getGradebook();
-        m.addAttribute("students",collegeStudents);
+        m.addAttribute("students", collegeStudents);
         return "index";
     }
 
@@ -29,69 +29,101 @@ public class GradebookController {
     @GetMapping("/studentInformation/{id}")
     public String studentInformation(@PathVariable int id, Model m) {
 
-        if (!studentService.checkIfStudentIsNull(id))
-        {
+        if (!studentService.checkIfStudentIsNull(id)) {
             return "error";
         }
 
         GradebookCollegeStudent studentEntity = studentService.studentInformation(id);
         m.addAttribute("student", studentEntity);
-        if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0)
-        {
+        if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
             m.addAttribute("mathAverage", studentEntity.getStudentGrades().findGradePointAverage(
                     studentEntity.getStudentGrades().getMathGradeResults()
             ));
-        } else
-        {
-            m.addAttribute("mathAvarage","N/A");
+        } else {
+            m.addAttribute("mathAvarage", "N/A");
         }
-        if (studentEntity.getStudentGrades().getScienceGradeResults().size() > 0)
-        {
+        if (studentEntity.getStudentGrades().getScienceGradeResults().size() > 0) {
             m.addAttribute("scienceAverage", studentEntity.getStudentGrades().findGradePointAverage(
                     studentEntity.getStudentGrades().getScienceGradeResults()
             ));
         } else {
-            m.addAttribute("scienceAverage","N/A");
+            m.addAttribute("scienceAverage", "N/A");
         }
-        if (studentEntity.getStudentGrades().getHistoryGradeResults().size() > 0)
-        {
-         m.addAttribute("historyAverage", studentEntity.getStudentGrades().findGradePointAverage(
-                 studentEntity.getStudentGrades().getHistoryGradeResults()
-         ));
-        }   else {
-            m.addAttribute("historyAverage","N/A");
+        if (studentEntity.getStudentGrades().getHistoryGradeResults().size() > 0) {
+            m.addAttribute("historyAverage", studentEntity.getStudentGrades().findGradePointAverage(
+                    studentEntity.getStudentGrades().getHistoryGradeResults()
+            ));
+        } else {
+            m.addAttribute("historyAverage", "N/A");
         }
         return "studentInformation";
     }
 
 
     @PostMapping(value = "/")
-    public String createStudent(@ModelAttribute("student") CollegeStudent student, Model m)
-    {
+    public String createStudent(@ModelAttribute("student") CollegeStudent student, Model m) {
 
         studentService.createStudent(student.getFirstname(), student.getLastname(),
                 student.getEmailAddress());
 
         Iterable<CollegeStudent> collegeStudents = studentService.getGradebook();
-        m.addAttribute("students",collegeStudents);
+        m.addAttribute("students", collegeStudents);
         return "index";
     }
 
 
     @GetMapping("/delete/student/{id}")
-    public String deleteStudent(@PathVariable int id, Model m)
-    {
+    public String deleteStudent(@PathVariable int id, Model m) {
 
-        if(!studentService.checkIfStudentIsNull(id))
-        {
+        if (!studentService.checkIfStudentIsNull(id)) {
             return "error";
         }
 
         studentService.deleteStudent(id);
         Iterable<CollegeStudent> collegeStudents = studentService.getGradebook();
-        m.addAttribute("students",collegeStudents);
+        m.addAttribute("students", collegeStudents);
         return "index";
     }
+
+    @PostMapping("/grades")
+    public String createGrade(@RequestParam("grade") double grade,
+                              @RequestParam("gradeType") String gradeType,
+                              @RequestParam("studentId") int studentId,
+                              Model m) {
+        if (!studentService.checkIfStudentIsNull(studentId)) {
+            return "error";
+        }
+        boolean success = studentService.createGrade(grade, studentId, gradeType);
+        if (!success) {
+            return "error";
+        }
+        GradebookCollegeStudent studentEntity = studentService.studentInformation(studentId);
+
+        m.addAttribute("student", studentEntity);
+        if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
+            m.addAttribute("mathAverage", studentEntity.getStudentGrades().findGradePointAverage(
+                    studentEntity.getStudentGrades().getMathGradeResults()
+            ));
+        } else {
+            m.addAttribute("mathAvarage", "N/A");
+        }
+        if (studentEntity.getStudentGrades().getScienceGradeResults().size() > 0) {
+            m.addAttribute("scienceAverage", studentEntity.getStudentGrades().findGradePointAverage(
+                    studentEntity.getStudentGrades().getScienceGradeResults()
+            ));
+        } else {
+            m.addAttribute("scienceAverage", "N/A");
+        }
+        if (studentEntity.getStudentGrades().getHistoryGradeResults().size() > 0) {
+            m.addAttribute("historyAverage", studentEntity.getStudentGrades().findGradePointAverage(
+                    studentEntity.getStudentGrades().getHistoryGradeResults()
+            ));
+        } else {
+            m.addAttribute("historyAverage", "N/A");
+        }
+        return "studentInformation";
+    }
+
 
 
 

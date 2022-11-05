7b463cbcf19998db9ba17ea82c4f655d0950c67e
lewisc99 <lewiscontato99@gmail.com>
2022-11-04 21:51:35 -0300

    128. Testing Spring Boot REST APIs - Get Student Information

--- 5-springboot-restapi-student-grade/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 5-springboot-restapi-student-grade/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -28,6 +28,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
+import java.util.Optional;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -181,6 +183,22 @@ public class GradebookControllerTest {
                 .andExpect(jsonPath("$.status", is(404)))
                 .andExpect(jsonPath("$.message", is("Student or Grade was not found")));
 
+    }
+
+    @Test
+    public void studentInformationHttpRequest() throws Exception
+    {
+        Optional<CollegeStudent> student = studentDao.findById(1);
+
+        assertTrue(student.isPresent());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/studentInformation/{id}",1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.firstname", is("Eric")))
+                .andExpect(jsonPath("$.lastname",is("Roby")))
+                .andExpect(jsonPath("$.emailAddress",is("eric.roby@luv2code_school.com")));
     }
 
     @AfterEach

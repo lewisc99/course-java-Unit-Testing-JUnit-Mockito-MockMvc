36c883241a52bd9052b9e0f00022ad0eceb866bf
lewisc99 <lewiscontato99@gmail.com>
2022-11-04 21:43:57 -0300

    127. Testing Spring Boot REST APIs - Delete Student when Student ID Does Not Exist

--- 5-springboot-restapi-student-grade/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 5-springboot-restapi-student-grade/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -33,6 +33,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import static org.hamcrest.Matchers.is;
 
 @TestPropertySource("/application-test.properties")
 @SpringBootTest
@@ -171,6 +172,16 @@ public class GradebookControllerTest {
         assertFalse(studentDao.findById(1).isPresent());
     }
 
+    @Test
+    public void deleteStudentHttpRequestErrorPage() throws Exception {
+        assertFalse(studentDao.findById(0).isPresent());
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/student/{id}", 0))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status", is(404)))
+                .andExpect(jsonPath("$.message", is("Student or Grade was not found")));
+
+    }
 
     @AfterEach
     public void setupAfterTransaction() {
